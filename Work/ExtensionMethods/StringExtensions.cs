dfb478bdf5ae01462d758727257a521a1f2af739
Cody Owens <cowens1435@gmail.com>
2025-11-13 13:52:19 -0700

    StringExtensions Assignment; Before new stuff 11/13/2025

---
+++ Work/ExtensionMethods/StringExtensions.cs
@@ -0,0 +1,83 @@
+﻿namespace Extensions
+{
+    // Only here to get rid of errors, nothing actually runs here
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+
+        }
+    }
+
+    public static class StringExtensions
+    {
+        public static string ToPigLatin(this string s)
+        {
+            if (string.IsNullOrWhiteSpace(s)) return s;
+
+            string[] words = s.Split(' ');
+            List<string> pigLatinWords = new();
+
+            foreach (string word in words)
+            {
+                if (string.IsNullOrEmpty(word) || word.Length == 1)
+                {
+                    pigLatinWords.Add(word);
+                    continue;
+                }
+
+                bool isFirstUpper = char.IsUpper(word[0]);
+                string rest = word.Substring(1);
+                string pigWord = rest + char.ToLower(word[0]) + "ay";
+
+                if (isFirstUpper) pigWord = char.ToUpper(pigWord[0]) + pigWord.Substring(1);
+                pigLatinWords.Add(pigWord);
+            }
+            return string.Join(" ", pigLatinWords);
+        }
+
+        public static int WordCount(this string s)
+        {
+            return s.Split(" ").Length;
+        }
+
+        public static string ReverseWord(this string s)
+        {
+            string s1 = "";
+            for (int i = s.Length - 1; i >= 0; i--)
+            {
+                s1 += s[i];
+            }
+            return s1;
+        }
+
+        public static string ReverseWordOrder(this string s)
+        {
+            string[] words = s.Split(" ");
+            return string.Join(" ", words.Reverse());
+        }
+
+        public static bool IsPalindrome(this string s)
+        {
+            string s1 = "";
+            foreach (char c in s)
+            {
+                // Removing spaces
+                if (c != ' ') s1 += c;
+            }
+            return s1.ReverseWord().ToLower() == s1.ToLower();
+        }
+
+        public static string FlipCasing(this string s)
+        {
+            string s1 = ""; char c1 = ' ';
+            foreach (char c in s)
+            {
+                if (char.IsLower(c)) c1 = char.ToUpper(c);
+                else c1 = char.ToLower(c);
+                s1 += c1;
+            }
+            return s1;
+        }
+    }
+}
---
+++ Work/ExtensionsTests/UnitTest1.cs
@@ -0,0 +1,58 @@
+﻿using Extensions;
+
+namespace ExtensionsTests
+{
+    public class StringExtensionTests
+    {
+        [Theory]
+        [InlineData("Hello World", "Ellohay Orldway")]
+        public void ToPigLatin_ReturnsCorrectString(string m, string expected)
+        {
+            string actual = m.ToPigLatin();
+            Assert.Equal(expected, actual);
+        }
+
+        [Theory]
+        [InlineData("This is a test string", 5)]
+        [InlineData("Testing with, punctuation. things", 4)]
+        public void WordCount_ReturnsCorrectInt(string m, int expected)
+        {
+            int actual = m.WordCount();
+            Assert.Equal(actual, expected);
+        }
+
+        [Theory]
+        [InlineData("It shouldn't care about punctuation!", "!noitautcnup tuoba erac t'ndluohs tI")]
+        public void ReverseWord_ReturnsCorrectString(string m, string expected)
+        {
+            string actual = m.ReverseWord();
+            Assert.Equal(expected, actual);
+        }
+
+        [Theory]
+        [InlineData("These are some words", "words some are These")]
+        public void ReverseWordOrder_ReturnsCorrectString(string m, string expected)
+        {
+            string actual = m.ReverseWordOrder();
+            Assert.Equal(expected, actual);
+        }
+
+        [Theory]
+        [InlineData("racecar", true)]
+        [InlineData("Too hot to hoot", true)]
+        [InlineData("Me, myself, and I", false)]
+        public void IsPalindrome_ReturnsCorrectBool(string m, bool expected)
+        {
+            bool actual = m.IsPalindrome();
+            Assert.Equal(expected, actual);
+        }
+
+        [Theory]
+        [InlineData("HelLo All YoU BeaUTIFUL People", "hELlO aLL yOu bEAutiful pEOPLE")]
+        public void FlipCasing_ReturnsCorrectString(string m, string expected)
+        {
+            string actual = m.FlipCasing();
+            Assert.Equal(expected, actual);
+        }
+    }
+}
